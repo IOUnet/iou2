dbae9bddd73b375fb57e302c36d7fc4cb737a031
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-04-26 11:05:59 +0300

    feat: add MakeIOUToken2Page with styles

---
+++ src/pages/make-iou-token2-page/MakeIOUToken2Page.jsx
@@ -0,0 +1,78 @@
+import { Box, Typography, withStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import PageLayout from '../../components/page-layout/PageLayout';
+import Button from '../../components/button/Button';
+import Input from '../../components/input/Input';
+import styles from './styles';
+
+const MakeIOUToken2Page = ({ classes }) => {
+  const [input, setInput] = useState('India');
+  const handleButtonClick = () => console.log('button clicked');
+
+  return (
+    <PageLayout>
+      <Box className={classes.descriptionSection}>
+        <Typography className={classes.title} variant="subtitle1">
+          Make new IOU: Address
+        </Typography>
+
+        <Input
+          id={'Country'}
+          label={'Country'}
+          inputProps={{
+            onChange: (e) => setInput(e.target.value),
+            value: input,
+          }}
+        />
+
+        <Input
+          id={'State/Region'}
+          label={'State/Region'}
+          inputProps={{
+            // onChange: (e) => setInput(e.target.value),
+            // value: input,
+            defaultValue: ' Karnataka',
+          }}
+        />
+
+        <Input
+          id={'City/Town'}
+          label={'City/Town'}
+          inputProps={{
+            // onChange: (e) => setInput(e.target.value),
+            // value: input,
+            defaultValue: 'Bengaluru',
+          }}
+        />
+
+        <Input
+          id={'Street/Block'}
+          label={'Street/Block'}
+          inputProps={{
+            // onChange: (e) => setInput(e.target.value),
+            // value: input,
+            defaultValue: 'Bannerghatta Main Road',
+          }}
+        />
+
+        <Input
+          id={'Phone'}
+          label={'Phone'}
+          inputProps={{
+            // onChange: (e) => setInput(e.target.value),
+            // value: input,
+            type: 'tel',
+            defaultValue: '+91234567890',
+          }}
+        />
+      </Box>
+
+      <Box className={classes.actionSection}>
+        <Button onClick={handleButtonClick}>next 1/2</Button>
+        <Button onClick={handleButtonClick}>publish iou</Button>
+      </Box>
+    </PageLayout>
+  );
+};
+
+export default withStyles(styles, { withTheme: true })(MakeIOUToken2Page);
