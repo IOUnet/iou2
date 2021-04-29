d58b87f36420786e155d1a84a1a8499901e20669
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-04-29 12:46:15 +0300

    refact: add className prop to PageTitle

--- src/components/page-title/PageTitle.jsx
+++ src/components/page-title/PageTitle.jsx
@@ -1,10 +1,15 @@
 import { Typography, withStyles } from '@material-ui/core';
+import clsx from 'clsx';
 import React from 'react';
 import styles from './styles';
 
-const PageTitle = ({ classes, children, ...props }) => {
+const PageTitle = ({ classes, className, children, ...props }) => {
   return children && (
-    <Typography className={classes.root} variant="subtitle1" component="p" {...props}>
+    <Typography
+      className={clsx(classes.root, className)}
+      variant="subtitle1" component="p"
+      {...props}
+    >
       {children}
     </Typography>
   );
--- src/components/page-title/styles.js
+++ src/components/page-title/styles.js
@@ -3,7 +3,6 @@ import { createStyles, } from '@material-ui/core/styles';
 const styles = () =>
   createStyles({
     root: {
-      width: '100%',
       fontSize: '1.5rem',
       lineHeight: 1.167,
       whiteSpace: 'nowrap',
