ccd7e260890cf6bf84563267feca697c3e5b6a2a
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-04-29 15:06:14 +0300

    feat: add SelectDesiredIOUSwapPage

---
+++ src/pages/select-desired-iou-swap-page/SelectDesiredIOUSwapPage.jsx
@@ -0,0 +1,72 @@
+import { Box, Typography, withStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import PageLayout from '../../components/page-layout/PageLayout';
+import PageTitle from '../../components/page-title/PageTitle';
+import TextField from '../../components/textfield/TextField';
+import Checkbox from '../../components/checkbox/Checkbox';
+import Button from '../../components/button/Button';
+import styles from './styles';
+
+const SelectDesiredIOUSwapPage = ({ classes }) => {
+  const [keyword, setKeyword] = useState('select desired IOU swap');
+  const [search, setSearch] = useState(false);
+
+  const checkboxLabelText = 'Search in location';
+
+  return (
+    <PageLayout>
+      <Box className={classes.pageTitle}>
+        <PageTitle>Find desired IOU</PageTitle>
+      </Box>
+
+      <Box className={classes.controlsSection}>
+        <TextField
+          className={classes.textField_green}
+          id={'Keyword'}
+          label={'Keyword'}
+          onChange={(event) => setKeyword(event.target.value)}
+          value={keyword}
+        />
+
+        <Checkbox
+          checked={search}
+          id={checkboxLabelText}
+          label={
+            <Typography className={classes.checkbox_label}>{checkboxLabelText}</Typography>
+          }
+          onChange={(evt) => setSearch(evt.target.checked)}
+        />
+
+        <TextField
+          className={classes.textField_italic}
+          id={'Country'}
+          label={'Country'}
+          // onChange={(event) => setKeyword(event.target.value)}
+          // value={keyword}
+        />
+
+        <TextField
+          id={'State/Region'}
+          label={'State/Region'}
+          // onChange={(event) => setKeyword(event.target.value)}
+          // value={keyword}
+        />
+
+        <TextField
+          id={'City/Town'}
+          label={'City/Town'}
+          // onChange={(event) => setKeyword(event.target.value)}
+          // value={keyword}
+        />
+      </Box>
+
+      <Box className={classes.actionSection}>
+        <Button onClick={() => console.log('button clicked')}>
+          find iou
+        </Button>
+      </Box>
+    </PageLayout>
+  );
+};
+
+export default withStyles(styles, { withTheme: true })(SelectDesiredIOUSwapPage);
---
+++ src/pages/select-desired-iou-swap-page/SelectDesiredIOUSwapPage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SelectDesiredIOUSwapPage from './SelectDesiredIOUSwapPage';
+
+export default {
+  title: 'Pages/SelectDesiredIOUSwapPage',
+  component: SelectDesiredIOUSwapPage,
+};
+
+const Template = (args) => <SelectDesiredIOUSwapPage {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {};
