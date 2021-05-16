9f2f0e3523554177342a8772eb61d5d158de5527
Vital Raicheu <rvitaly1978@gmail.com>
2021-05-16 14:54:23 +0300

    feat: add links to swap pages buttons

--- src/pages/swap-select-available-token-page/SwapSelectAvailableTokenPage.jsx
+++ src/pages/swap-select-available-token-page/SwapSelectAvailableTokenPage.jsx
@@ -1,14 +1,26 @@
 import { Box, withStyles } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import PageLayout from '../../components/page-layout/PageLayout';
 import PageTitle from '../../components/page-title/PageTitle';
 import TokenCardsList from '../../components/token-cards-list/TokenCardsList';
 import Button from '../../components/button/Button';
+import { ROUTES } from '../../constants';
 import styles from './styles';
 
 import { cardListData } from '../../storybook-fake-data/storybook-fake-data';
 
 const SwapSelectAvailableTokenPage = ({ classes }) => {
+  const history = useHistory();
+
+  const handleSelectIOU = (_, id) => {
+    console.log('cardId ---', id);
+  };
+
+  const handleSwap = () => {
+    history.push(ROUTES.main);
+  };
+
   return (
     <PageLayout>
       <Box className={classes.pageTitle}>
@@ -18,12 +30,12 @@ const SwapSelectAvailableTokenPage = ({ classes }) => {
       <Box className={classes.listSection}>
         <TokenCardsList
           data={cardListData.slice(0, 2)}
-          onClick={() => console.log('card clicked')}
+          onClick={handleSelectIOU}
         />
       </Box>
 
       <Box className={classes.actionSection}>
-        <Button onClick={() => console.log('button clicked')}>
+        <Button onClick={handleSwap}>
           swap iou
         </Button>
       </Box>
