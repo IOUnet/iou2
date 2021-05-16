3462e355af57548fc4df0e504e99a56aacde6d45
Vital Raicheu <rvitaly1978@gmail.com>
2021-05-16 11:49:54 +0300

    feat: add router to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import React from 'react';
+import HomePage from './pages/home-page/HomePage';
+import { ROUTES } from './constants';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        App
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route component={HomePage} exact path={ROUTES.main} />
+          <Redirect to={ROUTES.main} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
