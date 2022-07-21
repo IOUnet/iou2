a796204a9bf3d5c49659e820c6743eda197034a7
Stan <st.takt@gmail.com>
2022-07-21 07:58:11 +0300

    SuretyIOU UX functional requiremets

---
+++ contracts/SuretyIOU.sol
@@ -0,0 +1,26 @@
+pragma solidity>= 0.8.0;
+pragma experimental ABIEncoderV2;
+import  "./IOUtoken.sol";
+
+contract SuretyIOU is IOUtoken {
+
+    /* Mutual Surety 
+    
+Alice, the artist, and designer with, a beautiful hairstyle, participates in a tender of Bob, the owner of several barbershops. She is young and ambitious but is a newbie in barbershops interior design. Bob doubts  Alice has enough experience and resources to  redesign all of his saloons. To ensure Bob of her ability,
+1. Alice emits for this project SureIOU tokens with amount, date & time of mature and asks her friends and partners for surety: photographer Phil, decorator Denniz, carpenter Calvin.
+1.1. Alice transfers them her own IOUs tokens (not this project SureIOU), equivalent in her amount of work, to ensure them that she will fullfill  her responsibilities to Bob.
+1.2. Phil, Dennis, and Calvin decide to support Alice with this project and transfer their IOUs to Alice’s SureIOU in the amount of work, they  pledge for Bob’s project.
+2. Alice transfers SureIOU to Bob’s security deposit,
+2.1. he sees all sureties of the project and decides to hire Alice,
+2.2. transfer prepay to her.
+3. Then Alice goes to suppliers of repair products and pays for their goods with this SureIOU tokens.
+4. When they successfully finished work, Bob pays them money (or his IOUs tokens) and
+5. Bob burns  SureIOUs provided by Alice.
+6. Alice pays
+6.1. to team their reward
+6.2. to supplier Stephan
+7. Final
+7.1. teammates  burn Alice’s IOUs and rate work win her
+7.2. Supplier Stephan  burn SureIOUs that he  holds too and rate this work too.
+ */
+}
